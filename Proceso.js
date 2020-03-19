d08ced100c03adcf3f73ee3904e086250543ce8d
Bernardo Cardenas Domene <bernardocardenas98@gmail.com>
2020-03-18 18:47:52 -0600

    txt

--- Proceso.js
+++ Proceso.js
@@ -1,18 +1,7 @@
 class Proceso {
-	constructor(
-		nombre,
-		tiempoLlegada,
-		tiempoActual,
-		ejecucionTotal,
-		quantumRestante,
-		paginas
-	) {
+	constructor(nombre, tiempoLlegada, paginas, ejecucionTotal) {
 		this.nombre = nombre;
 		this.tiempoLlegada = tiempoLlegada;
-		this.cpuAsignado = 0;
-		this.envejecimiento = tiempoActual - tiempoLlegada - 0;
-		this.cpuRestante = ejecucionTotal;
-		this.quantumRestante = quantumRestante;
 		this.paginas = paginas;
 		this.ejecucionTotal = ejecucionTotal;
 	}
@@ -21,43 +10,6 @@ class Proceso {
 		const div = document.createElement('div');
 		div.classList.add('my-2', 'border');
 		div.innerHTML = this.nombre;
-		return div;
-	}
-
-	toRunningDiv() {
-		const div = document.createElement('div');
-		div.classList.add('my-2', 'border', 'bg-success');
-		div.innerHTML = this.nombre;
-		return div;
-	}
-
-	toSummaryDiv() {
-		const div = document.createElement('div');
-
-		const nombre = document.createElement('p');
-		nombre.innerHTML = 'Nombre: ' + this.nombre;
-		div.appendChild(nombre);
-
-		const tiempoLlegada = document.createElement('p');
-		tiempoLlegada.innerHTML = 'Tiempo Llegada: ' + this.tiempoLlegada;
-		div.appendChild(tiempoLlegada);
-
-		const cpuAsignado = document.createElement('p');
-		cpuAsignado.innerHTML = 'CPU Asignado: ' + this.cpuAsignado;
-		div.appendChild(cpuAsignado);
-
-		const envejecimiento = document.createElement('p');
-		envejecimiento.innerHTML = 'Envejecimiento: ' + this.envejecimiento;
-		div.appendChild(envejecimiento);
-
-		const cpuRestante = document.createElement('p');
-		cpuRestante.innerHTML = 'CPU Restante: ' + this.cpuRestante;
-		div.appendChild(cpuRestante);
-
-		const quantumRestante = document.createElement('p');
-		quantumRestante.innerHTML = 'Quantum Restante: ' + this.quantumRestante;
-		div.appendChild(quantumRestante);
-
 		return div;
 	}
 
--- index.js
+++ index.js
@@ -1,33 +1,108 @@
+let data = {
+	numeroPaginas: 0,
+	tiempoActual: 0,
+	numeroDeProcesos: 0,
+	procesos: []
+}
+
+
+
 document
 	.getElementById('agregar-proceso-form')
-	.addEventListener('submit', e => agregarProceso(e));
-
-let tiempoActual = 0;
-let numeroProcesos = 0;
-let quantumAsignado = 5;
-let procesosReady = [];
-let procesoRunning = null;
-let procesosBlocked = [];
-let procesosFinished = [];
-
-function agregarProceso(form) {
-	form.preventDefault();
-	nuevoProceso = crearProceso(form);
-	organizarProceso(nuevoProceso);
-}
+	.addEventListener('submit', e => getProcessProperties(e));
 
 function ejecutarInstruccion() {
-	tiempoActual += 1;
+	const tiempoActual =
+		Number(document.getElementById('tiempo-actual').innerHTML) + 1;
+
 	document.getElementById('tiempo-actual').innerHTML = tiempoActual;
-	actualizarRunningProceso();
 }
 
-function actualizarRunningProceso() {
-	procesoRunning.cpuAsignado += 1;
+function getProcessProperties(e) {
+	e.preventDefault();
+	nuevoProceso = new Proceso(
+		e.target.elements[0].value,
+		document.getElementById('tiempo-actual').innerHTML,
+		e.target.elements[1].value,
+		e.target.elements[2].value
+	);
+	displayProcess(nuevoProceso);
 }
 
-function renderRunningSummary() {
-	document
-		.getElementById('running-proceso-summary')
-		.appendChild(procesoRunning.toSummaryDiv());
+function displayProcess(nuevoProceso) {
+	ejecutarInstruccion();
+	// let readyProcesos = document.getElementById('ready-procesos').children;
+	// let addReadyProcesos = [];
+
+	// if (readyProcesos.length > 0) {
+	// 	for (let i = 0; i < readyProcesos.length; i++)
+	// 		addReadyProcesos.push(readyProcesos[i]);
+	// }
+
+	// addReadyProcesos.push(nuevoProceso.toDiv());
+
+	// readyProcesos = document.getElementById('ready-procesos');
+	// for (let i = 0; i < addReadyProcesos; i++) {
+	// 	readyProcesos.appendChild(addReadyProcesos[i]);
+	// }
+
+	let readyProcesos = document
+		.getElementById('ready-procesos')
+		.appendChild(nuevoProceso.toDiv());
+
+	console.log(document.getElementById('ready-procesos'));
+}
+
+function cargarTxt() {
+	console.log('cargando txt');
+	const file = document.getElementById('input-file').files[0]
+	const reader = new FileReader
+	reader.addEventListener('load', () => {
+		txt = reader.result;
+		txt = txt.split('\n');
+		linea1 = txt[0].split(',');
+		data.numeroPaginas = linea1[0];
+		data.tiempoActual = linea1[1];
+		data.numeroDeProcesos = txt[1];
+		let lineaActual = 2;
+		let proceso = {};
+		for(var i=0; i<data.numeroDeProcesos; i++) { //crear un proceso
+			let datosCpuTxt = txt[lineaActual].split(',');
+			lineaActual++
+			let nPaginas = txt[lineaActual];
+
+			let objMemoria = {
+				residencia: [],
+				llegada: [],
+				ultimoAcceso: [],
+				numeroDeAccesos: [],
+				NUR: []
+			};
+
+			for(var j=0; j<nPaginas; j++) {
+				lineaActual++;
+				linea = txt[lineaActual];
+				linea = linea.split(',');
+				objMemoria.residencia.push(linea[0]);
+				objMemoria.llegada.push(linea[1]);
+				objMemoria.ultimoAcceso.push(linea[2]);
+				objMemoria.numeroDeAccesos.push(linea[3]);
+				objMemoria.NUR.push(linea[4]+''+linea[5]);
+			};
+			lineaActual ++;
+
+			proceso = {
+				datosCpu: {
+					llegada: datosCpuTxt[0],
+					tiempoTotalEstimado: datosCpuTxt[1],
+					estado: datosCpuTxt[2]
+				},
+				numeroPaginas: nPaginas,
+				datosMemoria: objMemoria
+			};
+			data.procesos.push(proceso);
+		};
+	});
+	reader.readAsText(file, 'UTF-8')
+	console.log(data);
 }
